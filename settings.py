79955f381dc232cc6a51fc8724e3c2c4ab62518c
sumikof <ssumikof187@gmail.com>
2022-01-18 22:53:10 +0900

    make requirements.txt

--- settings.py
+++ settings.py
@@ -1,14 +1,11 @@
 import os
 import yaml
-from os.path import expanduser
 
-home = expanduser("~")
-
-with open(os.path.join(home, f'.boto3.yaml')) as yaml_file:
-    boto3_config = yaml.safe_load(yaml_file)
 
 TOP_DIR = os.path.dirname(__file__)
 ENVIRONMENT = os.getenv('ENVIRONMENT', default='dev')
+with open(os.path.join(TOP_DIR, 'config', f'secret.yaml')) as yaml_file:
+    boto3_config = yaml.safe_load(yaml_file)
 
 with open(os.path.join(TOP_DIR, 'config', f'config_{ENVIRONMENT}.yaml')) as yaml_file:
     config = yaml.safe_load(yaml_file)
